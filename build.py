45c93abf89654232db4a75a5862c89ce564edd1f
Darren Kearney <darren@darrenk.net>
2018-04-19 14:54:35 +0100

    Modified build script - a quick refactor to tidy things up

--- build.py
+++ build.py
@@ -5,6 +5,90 @@
     Author: Darren Kearney
     Author URI: https://darrenk.net
 """
+def main():
+
+    includes = get_includes()
+    chunks = get_chunks_from_includes( includes )
+    output = prepare_output( chunks )
+    write_output_to_file( output, 'bin/jamthegame.p8' )
+
+def write_output_to_file( output, filepath = 'bin/build.p8' ):
+    # Write output to file
+    with open( filepath , 'w' ) as build:
+        build.write( output )
+
+    print("Done.")
+
+
+def get_includes():
+    print("Reading includes file")
+    with open('includes', 'r') as f:
+        read_data = f.read()
+
+    includes = {}
+
+    for i in read_data.split():
+        k=i.split('=')[0]
+        v=i.split('=')[1]
+        includes[k] = v
+
+    return includes
+
+
+def get_chunks_from_includes( includes = {} ):
+    chunks = {
+        '__pre__': '',
+        '__gfx__': '',
+        '__gff__': '',
+        '__lua__': '',
+        '__map__': '',
+        '__sfx__': '',
+        '__music__': ''
+    }
+
+    # Go into each include file
+    for filepath in includes.values():
+        print(" + {}".format( filepath ))
+
+        if filepath == includes['__pre__']: 
+            # Read peamble
+            with open(filepath, 'r') as p:
+                preamble_data = p.read()
+            chunks['__pre__'] = "{}".format(preamble_data)
+
+        if filepath == includes['__lua__']:
+            chunks['__lua__'] = pico8_get_chunk_from_file( "__lua__", filepath ) 
+
+        if filepath == includes['__gfx__']:
+            chunks['__gfx__'] = pico8_get_chunk_from_file( "__gfx__", filepath )
+            chunks['__gff__'] = pico8_get_chunk_from_file( "__gff__", filepath )
+            chunks['__map__'] = pico8_get_chunk_from_file( "__map__", filepath )
+     
+        if filepath == includes['__sfx__']:
+            chunks['__sfx__'] = pico8_get_chunk_from_file( "__sfx__", filepath )
+            chunks['__music__'] = pico8_get_chunk_from_file( "__music__", filepath )
+
+    return chunks
+
+
+def prepare_output(chunks):
+    output = ""
+    
+    def stitch_chunks(output, label):
+        return output + "\n{}\n{}".format(label, chunks[label])
+    
+    output += chunks['__pre__']                  #  Preamble / file header
+    output = stitch_chunks(output, '__lua__')    #  Lua code
+    output = stitch_chunks(output, '__gfx__')    #  Sprites
+    output = stitch_chunks(output, '__gff__')    #
+    output = stitch_chunks(output, '__map__')    #  Map data
+    output = stitch_chunks(output, '__sfx__')    #  Audio data
+    output = stitch_chunks(output, '__music__')  #  Music data
+    
+    return output
+
+
+
 
 def pico8_get_chunk_from_file( label, filepath ):
     
@@ -38,76 +122,4 @@ def pico8_get_chunk_from_file( label, filepath ):
 
     return chunk
 
-print("Reading includes file")
-with open('includes', 'r') as f:
-    read_data = f.read()
-
-includes = {}
-
-for i in read_data.split():
-    k=i.split('=')[0]
-    v=i.split('=')[1]
-    includes[k] = v
-
-chunks = {
-    '__pre__': '',
-    '__gfx__': '',
-    '__gff__': '',
-    '__lua__': '',
-    '__map__': '',
-    '__sfx__': '',
-    '__music__': ''
-}
-
-
-# Go into each include file
-for filepath in includes.values():
-    print(" + {}".format( filepath ))
-
-    if filepath == includes['__pre__']: 
-        # Read peamble
-        with open(filepath, 'r') as p:
-            preamble_data = p.read()
-        chunks['__pre__'] = "{}".format(preamble_data)
-
-    if filepath == "src/lua/lua.p8":
-        chunks['__lua__'] = pico8_get_chunk_from_file( "__lua__", filepath ) 
-
-
-    if filepath == "src/assets/gfx.p8":
-        chunks['__gfx__'] = pico8_get_chunk_from_file( "__gfx__", filepath )
-        chunks['__gff__'] = pico8_get_chunk_from_file( "__gff__", filepath )
-        chunks['__map__'] = pico8_get_chunk_from_file( "__map__", filepath )
- 
-    if filepath == "src/assets/sfx.p8":
-        chunks['__sfx__'] = pico8_get_chunk_from_file( "__sfx__", filepath )
-        chunks['__music__'] = pico8_get_chunk_from_file( "__music__", filepath )
-
-# print(chunks)
-
-# Stitch the output strings together
-
-output = ""
-
-def stitch_chunks(output, label):
-    output += "{}\n{}".format(label, chunks[label])
-
-# print(output)
-
-## Preamble
-output += chunks['__pre__']
-# Lua code
-output += "\n__lua__\n{}".format(chunks['__lua__'])
-# Art assets
-output += "\n__gfx__\n{}".format(chunks['__gfx__'])
-output += "\n__gff__\n{}".format(chunks['__gff__'])
-output += "\n__map__\n{}".format(chunks['__map__'])
-# Audio assets
-output += "\n__sfx__\n{}".format(chunks['__sfx__'])
-output += "\n__music__\n{}".format(chunks['__music__'])
-
-# Append all strings together for build
-with open('bin/jamthegame.p8', 'w') as build:
-    build.write(output)
-
-print("Done.")
+main()
